0930feed92ad8e3f93b13fe3a174fc8a7481c900
Mitesh Shah <mitesh@miteshshah.com>
2022-03-22 11:53:57 +0530

    fix blog file messup

--- src/quest/guide/components/blog.clj
+++ src/quest/guide/components/blog.clj
@@ -1,7 +1,24 @@
 (ns quest.guide.components.blog
   (:require [lambdaisland.ornament :as o :refer [defstyled]]))
 
-(defstyled list :section
+(defstyled prose :div
+  :text-lg :leading-normal
+  [:p :mt-5]
+  [:h1 :border-b-1 :border-gray-200 :pb-2 :mt-4 :font-fancy-bold :text-5xl]
+  [:h2 :border-b-1 :border-gray-200 :pb-2 :mt-4 :font-fancy-bold :text-3xl]
+  [:h3 :border-b-1 :border-gray-200 :pb-2 :mt-4 :font-fancy-bold :text-xl]
+  [:h4 :border-b-1 :border-gray-200 :pb-2 :mt-4 :font-fancy-bold :text-lg]
+  [:h5 :border-b-1 :border-gray-200 :pb-2 :mt-4 :font-fancy-bold :text-base]
+  [:h6 :border-b-1 :border-gray-200 :pb-2 :mt-4 :font-fancy-bold :text-sm]
+  [:ul :list-disc :list-inside :mt-4]
+  [:li :mt-2]
+  [:a :text-teal-600]
+  [:blockquote
+   :mt-4 :pl-4 :border-l-5 :border-teal-600 :py-4 :pr-3 :bg-teal-50
+   [:>p:first-child :mt-0]]
+  )
+
+(defstyled index :section
   :m-4 :mt-12
   [:>h2 :font-fancy-bold :text-4xl]
   [:>ul :list-disc
@@ -22,6 +39,10 @@
         ])]]))
 
 (defstyled entry :div
+  :m-4 :mt-12
+  [:>h1 :font-fancy-bold :text-4xl]
+  [:.post-meta :mt-4]
+  [:>main :my-4]
   ([{:keys [meta html] :as post}]
    [:<>
     [:h1 (:title meta)]
@@ -30,5 +51,6 @@
      [:span (.format (java.text.SimpleDateFormat. "MMM dd, yyyy") (:date meta))]
      " by "
      [:span (:author meta)]]
-    [:lambdaisland.hiccup/unsafe-html html]])
+    [prose
+     [:lambdaisland.hiccup/unsafe-html html]]])
   )
